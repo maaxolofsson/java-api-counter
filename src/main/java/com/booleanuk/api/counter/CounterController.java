b9860bd683fd0e0b65a0cbc3f30e78b320754e8a
Max V Olofsson <max.olofsson@se.experis.com>
2024-08-28 11:13:19 +0200

    Extension done

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,18 +1,19 @@
 package com.booleanuk.api.counter;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController()
 @RequestMapping("/counter")
 public class CounterController {
 
-    private int counter = 0;
+    private HashMap<String, Integer> counters;
+    private int counter;
 
     public CounterController() {
-
+        this.counters = new HashMap<>();
+        this.counter = 0;
     }
 
     @GetMapping("increment")
@@ -30,6 +31,38 @@ public class CounterController {
     @GetMapping
     public int getCounter() {
         return this.counter;
+    }
+
+    @GetMapping("custom/{name}")
+    public int getCustomCounter(@PathVariable String name) {
+        if (this.counters.get(name) == null) {
+            this.counters.put(name, 0);
+            return this.counters.get(name);
+        } else {
+            return this.counters.get(name);
+        }
+    }
+
+    @GetMapping("custom/{name}/increment")
+    public int getCustomCounterIncrement(@PathVariable String name) {
+        if (this.counters.get(name) == null) {
+            this.counters.put(name, 1);
+            return this.counters.get(name);
+        } else {
+            this.counters.replace(name, this.counters.get(name) + 1);
+            return this.counters.get(name);
+        }
+    }
+
+    @GetMapping("custom/{name}/decrement")
+    public int getCustomCounterDecrement(@PathVariable String name) {
+        if (this.counters.get(name) == null) {
+            this.counters.put(name, -1);
+            return this.counters.get(name);
+        } else {
+            this.counters.replace(name, this.counters.get(name) - 1);
+            return this.counters.get(name);
+        }
     }
 
 }
