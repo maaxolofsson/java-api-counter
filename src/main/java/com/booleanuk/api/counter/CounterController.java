67506c15e49d1e7db4219413880ec8ccd66ac57e
Max V Olofsson <max.olofsson@se.experis.com>
2024-08-28 11:04:39 +0200

    Core done.

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,5 +1,35 @@
 package com.booleanuk.api.counter;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/counter")
 public class CounterController {
+
+    private int counter = 0;
+
+    public CounterController() {
+
+    }
+
+    @GetMapping("increment")
+    public int increment() {
+        ++this.counter;
+        return this.counter;
+    }
+
+    @GetMapping("decrement")
+    public int decrement() {
+        --this.counter;
+        return this.counter;
+    }
+
+    @GetMapping
+    public int getCounter() {
+        return this.counter;
+    }
 
 }
